64131b37d2b4bb7a2306c625f482ff4748b9753d
Gustavo <gw79245@gmail.com>
2025-05-06 00:15:31 -0300

    Adiciona a logica do semaforo

--- semaforo/ControladorSemaforos.java
+++ semaforo/ControladorSemaforos.java
@@ -1,7 +1,23 @@
 package semaforo;
 
-public class ControladorSemaforos {
+import semaforo.listener.Listener;
+
+public class ControladorSemaforos implements Listener {
+
+    Semaforo semaforo = new Semaforo(4, 2, 6);
+
     public void aplicarControle() {
         System.out.println("Controle de semáforos aplicado.");
+    }
+
+    @Override
+    public void aoDispararEvento(String tipoEvento, Object dados) {
+        if (tipoEvento.equals("TICK")) {
+            int tempo = (int) dados;
+            System.out.println("[Controlador] Verificando semáforos no minuto " + tempo);
+
+            semaforo.atualizar(tempo);
+            semaforo.exibirEstado();
+        }
     }
 }
--- semaforo/Semaforo.java
+++ semaforo/Semaforo.java
@@ -1,17 +1,38 @@
 package semaforo;
 
 public class Semaforo {
+
+    public enum Cor {
+        VERDE, AMARELO, VERMELHO
+    }
+
     private int tempoVerde;
     private int tempoAmarelo;
     private int tempoVermelho;
+
+    private Cor corAtual;
 
     public Semaforo(int verde, int amarelo, int vermelho) {
         this.tempoVerde = verde;
         this.tempoAmarelo = amarelo;
         this.tempoVermelho = vermelho;
+        this.corAtual = Cor.VERDE;
+    }
+
+    public void atualizar(int tempo) {
+        int cicloTotal = tempoVerde + tempoAmarelo + tempoVermelho;
+        int tempoNoCiclo = tempo % cicloTotal;
+
+        if (tempoNoCiclo < tempoVerde) {
+            corAtual = Cor.VERDE;
+        } else if (tempoNoCiclo < tempoVerde + tempoAmarelo) {
+            corAtual = Cor.AMARELO;
+        } else {
+            corAtual = Cor.VERMELHO;
+        }
     }
 
     public void exibirEstado() {
-        System.out.println("Semáforo em execução.");
+        System.out.println("Semáforo está " + corAtual);
     }
 }
